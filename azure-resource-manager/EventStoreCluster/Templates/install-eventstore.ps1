385046c0ac5078f5f126f2c268f688312de3d91a
Phil Bolduc <philbolduc@sierrasystems.com>
2016-01-07 11:38:08 -0800

    Changed paths to eventstore and nssm.

--- azure-resource-manager/EventStoreCluster/Templates/install-eventstore.ps1
+++ azure-resource-manager/EventStoreCluster/Templates/install-eventstore.ps1
@@ -12,6 +12,10 @@ param (
     $IntIp,
     [string]
     $ExtIp,
+	[string]
+	$ExtIpAdvertiseAs,
+	[Int]
+	$ClusterSize = 1,
     [Int]
     $IntTcpPort = 1112,
     [Int]
@@ -19,20 +23,15 @@ param (
     [Int]
     $IntHttpPort = 2112,
     [Int]
-    $ExtHttpPort = 2113,
-	[string]
-	$ExtIpAdvertiseAs
+    $ExtHttpPort = 2113
 
 )
 
-function Extract-ZipFile($file, $destination)
-{
-	#Add-Type -Assembly System.IO.Compression.FileSystem
-	#[System.IO.Compression.ZipFile]::ExtractToDirectory($file,$destination)
-
+function Extract-ZipFile($file, $destination) {
     if (![System.IO.Directory]::Exists($destination)) {
         [System.IO.Directory]::CreateDirectory($destination)
     }
+	
     $shell = new-object -com shell.application
     $zip = $shell.NameSpace($file)
     
@@ -55,10 +54,10 @@ New-Item $downloadDirectory -ItemType Directory | Out-Null
 
 $nssmZip = Download-FileTo -DownloadUrl $nssmDownloadUrl -Path $downloadDirectory
 # NSSM is packed with in a folder already
-Extract-ZipFile -File $nssmZip -Destination F:\apps\
+Extract-ZipFile -File $nssmZip -Destination F:\
 
 $eventStoreZip = Download-FileTo -DownloadUrl $EventStoreDownloadUrl -Path $downloadDirectory
-Extract-ZipFile -File $eventStoreZip -Destination F:\apps\eventstore\$EventStoreVersion\
+Extract-ZipFile -File $eventStoreZip -Destination F:\eventstore\bin\
 
 #
 #
@@ -77,37 +76,39 @@ $ipAddress = (Resolve-DnsName $env:COMPUTERNAME -Type A).IPAddress
 # Manager Node External HTTP Interface (commercial only)
 #netsh http add urlacl url=http://$ipAddress:30778/ user="NT AUTHORITY\LOCAL SERVICE"
 
-# Added all the ports, but think I only require the 2112,2113 ports
 New-NetFirewallRule -Name Allow_EventStore_Int_In `
 					-DisplayName "Allow inbound Internal Event Store traffic" `
 					-Protocol TCP `
 					-Direction Inbound `
 					-Action Allow `
-					-LocalPort ${IntTcpPort},${IntHttpPort}
+					-LocalPort $IntTcpPort,$ExtTcpPort
 
 New-NetFirewallRule -Name Allow_EventStore_Ext_In `
 					-DisplayName "Allow inbound External Event Store traffic" `
 					-Protocol TCP `
 					-Direction Inbound `
 					-Action Allow `
-					-LocalPort ${ExtTcpPort},${ExtHttpPort}
+					-LocalPort $IntHttpPort,$ExtHttpPort
 
--MemDb -Log D:\eventstore\logs -ClusterSize 3 -DiscoverViaDns false -GossipSeed %IP0%:2112,%IP1%:2112,%IP2%:2112
-
-EventStore.ClusterNode.exe %COMMON_OPTS% -IntIp %IP0% -ExtIp %IP0% -ExtIpAdvertiseAs %PIP0%
-EventStore.ClusterNode.exe %COMMON_OPTS% -IntIp %IP1% -ExtIp %IP1% -ExtIpAdvertiseAs %PIP1%
-EventStore.ClusterNode.exe %COMMON_OPTS% -IntIp %IP2% -ExtIp %IP2% -ExtIpAdvertiseAs %PIP2%
-					
-Add-Content F:\apps\eventstore\config.yaml "# default Event Store configuration file`n"
-Add-Content F:\apps\eventstore\config.yaml "Db:               F:\apps\eventstore\data`n"
-Add-Content F:\apps\eventstore\config.yaml "Log:              D:\eventstore\logs`n"
-Add-Content F:\apps\eventstore\config.yaml "IntIp:            $ipAddress`n"
-Add-Content F:\apps\eventstore\config.yaml "ExtIp:            $ipAddress`n"
-Add-Content F:\apps\eventstore\config.yaml "ExtIpAdvertiseAs: $ExtIpAdvertiseAs`n"
-Add-Content F:\apps\eventstore\config.yaml "ClusterSize:      3`n"
-Add-Content F:\apps\eventstore\config.yaml "DiscoverViaDns:   false`n"
-Add-Content F:\apps\eventstore\config.yaml "GossipSeed:       10.0.1.4:$IntHttpPort,10.0.1.5:$IntHttpPort,10.0.1.6:$IntHttpPort`n"
-
+# SET COMMON_OPTS=-MemDb -Log D:\eventstore\logs -ClusterSize 3 -DiscoverViaDns false -GossipSeed %IP0%:2112,%IP1%:2112,%IP2%:2112
 #
-C:\apps\nssm-2.24\win64\nssm.exe install EventStore F:\apps\eventstore\$EventStoreVersion\EventStore.ClusterNode.exe --config F:\apps\eventstore\config.yaml
-C:\apps\nssm-2.24\win64\nssm.exe set EventStore Description "The EventStore service"
+# EventStore.ClusterNode.exe %COMMON_OPTS% -IntIp %IP0% -ExtIp %IP0% -ExtIpAdvertiseAs %PIP0%
+# EventStore.ClusterNode.exe %COMMON_OPTS% -IntIp %IP1% -ExtIp %IP1% -ExtIpAdvertiseAs %PIP1%
+# EventStore.ClusterNode.exe %COMMON_OPTS% -IntIp %IP2% -ExtIp %IP2% -ExtIpAdvertiseAs %PIP2%
+					
+Add-Content F:\eventstore\config.yaml "# default Event Store configuration file`n"
+Add-Content F:\eventstore\config.yaml "Db:               F:\eventstore\data`n"
+Add-Content F:\eventstore\config.yaml "Log:              D:\eventstore\logs`n"
+Add-Content F:\eventstore\config.yaml "IntIp:            $ipAddress`n"
+Add-Content F:\eventstore\config.yaml "ExtIp:            $ipAddress`n"
+Add-Content F:\eventstore\config.yaml "ExtIpAdvertiseAs: $ExtIpAdvertiseAs`n"
+Add-Content F:\eventstore\config.yaml "ClusterSize:      $ClusterSize`n"
+Add-Content F:\eventstore\config.yaml "DiscoverViaDns:   false`n"
+Add-Content F:\eventstore\config.yaml "GossipSeed:       10.0.1.4:$IntHttpPort,10.0.1.5:$IntHttpPort,10.0.1.6:$IntHttpPort`n"
+
+Add-Content F:\eventstore\install-service.cmd "F:\nssm-2.24\win64\nssm.exe install EventStore F:\eventstore\bin\EventStore.ClusterNode.exe --config F:\eventstore\config.yaml"
+Add-Content F:\eventstore\install-service.cmd "F:\nssm-2.24\win64\nssm.exe set EventStore Description ""The EventStore service"""
+
+Add-Content F:\eventstore\start-service.cmd "net start EventStore"
+Add-Content F:\eventstore\stop-service.cmd "net stop EventStore"
+
