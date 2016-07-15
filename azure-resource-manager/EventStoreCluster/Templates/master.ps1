eea864af24cf82b796ffbfc34fdb6f15145a285b
Corey Perkins <corey.perkins@gmail.com>
2016-07-15 15:24:16 -0600

    Changed to double quotes to load the other script files

--- azure-resource-manager/EventStoreCluster/Templates/master.ps1
+++ azure-resource-manager/EventStoreCluster/Templates/master.ps1
@@ -7,7 +7,7 @@ Stop-Transcript | out-null
 $ErrorActionPreference = "Continue"
 Start-Transcript -path C:\ps-output-master.txt -append -noClobber
 
-& '..\create-data-disks.ps1'
-& '..\install-eventstore.ps1'
+& "..\create-data-disks.ps1"
+& "..\install-eventstore.ps1"
 
 Stop-Transcript
