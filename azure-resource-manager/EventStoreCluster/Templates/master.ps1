8918bba279773e727555fbab6577e73b3adf5706
Corey Perkins <corey.perkins@gmail.com>
2016-07-17 08:25:35 -0600

    temp parameters to test event store install

--- azure-resource-manager/EventStoreCluster/Templates/master.ps1
+++ azure-resource-manager/EventStoreCluster/Templates/master.ps1
@@ -8,6 +8,11 @@ $ErrorActionPreference = "Continue"
 Start-Transcript -path C:\ps-output-master.txt -append -noClobber
 
 . .\create-data-disks.ps1
-. .\install-eventstore.ps1
+
+# TODO: These parameters should come from the template!
+. .\install-eventstore.ps1 -EventStoreVersion "3.8.0" `
+						   -EventStoreDownloadUrl "http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.8.0.zip" `
+						   -nssmDownloadUrl "https://nssm.cc/release/nssm-2.24.zip" `
+						   -ClusterSize 3
 
 Stop-Transcript
