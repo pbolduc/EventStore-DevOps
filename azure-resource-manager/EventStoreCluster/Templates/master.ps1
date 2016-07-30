8ce8e70ef9edd67916b8e35ad50637c136f0aa67
Phil Bolduc <philbolduc@sierrasystems.com>
2016-07-30 11:58:28 -0700

    Pass ES version and URL to master provision script.

--- azure-resource-manager/EventStoreCluster/Templates/master.ps1
+++ azure-resource-manager/EventStoreCluster/Templates/master.ps1
@@ -1,6 +1,13 @@
 #
 # master.ps1
 #
+param(
+	[Int32]$ClusterSize,
+	[string]$esVer,
+	[string]$esUrl,
+	[string]$nginxVer="1.10.1",
+	[string]$nginxUrl="https://landdb.blob.core.windows.net/eventstore-cluster-resources/nginx-1.10.1.zip"
+)
 
 $ErrorActionPreference="SilentlyContinue"
 Stop-Transcript | out-null
@@ -13,14 +20,14 @@ New-Item $downloadDirectory -ItemType Directory | Out-Null
 . .\create-data-disks.ps1
 
 # TODO: These parameters should come from the template!
-. .\install-eventstore.ps1 -EventStoreVersion "3.8.0" `
-						   -EventStoreDownloadUrl "http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.8.0.zip" `
+. .\install-eventstore.ps1 -EventStoreVersion $esVer `
+						   -EventStoreDownloadUrl $esUrl `
 						   -nssmDownloadUrl "https://nssm.cc/release/nssm-2.24.zip" `
-						   -ClusterSize 3 `
+						   -ClusterSize $ClusterSize `
 						   -downloadDirectory $downloadDirectory
 
-. .\install-nginx.ps1 -NGinxVersion "1.10.1" `
-					  -NGinxDownloadUrl "https://landdb.blob.core.windows.net/eventstore-cluster-resources/nginx-1.10.1.zip" `
+. .\install-nginx.ps1 -NGinxVersion $nginxVer `
+					  -NGinxDownloadUrl $nginxUrl `
 					  -downloadDirectory $downloadDirectory
 
 Stop-Transcript
